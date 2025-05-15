7177900d3b3573a0bc9f88c6aa1bf6fe0a1df596
JoeEllis <joeellis.2013@gmail.com>
2025-05-15 15:38:58 -0400

    added gwinnett and dekalb county zip code geometries, tested the coordanate polygons on folium map

--- utils_choropleth.py
+++ utils_choropleth.py
@@ -1,9 +1,33 @@
 from shapely.geometry import Point, Polygon
 import pandas
 import geopandas
+import folium
+import matplotlib.pyplot as plt
 
 df_food = pandas.read_pickle('df_food_geocode.pkl')
 df_atl_neighborhoods = geopandas.read_file('Atlanta_Official_Neighborhood_Statistical_Areas_(NSA).geojson')
+
+#needed to  convert CRS to match geojson
+df_dekalb = geopandas.read_file('kx-dekalb-county-ga-zip-codes-SHP/dekalb-county-ga-zip-codes.shp').to_crs(crs=4326)
+df_gwin = geopandas.read_file('kx-gwinnett-county-ga-zip-code-SHP/gwinnett-county-ga-zip-code.shp').to_crs(crs=4326)
+
+
+###### test to see geometries
+#only grabs geo's from each county
+s = pandas.concat([df_atl_neighborhoods['geometry'], df_dekalb['geometry'], df_gwin['geometry']])
+df_geo = geopandas.GeoDataFrame(s, columns=['geometry'])
+
+#create folium map with atl center
+m = folium.Map(location=[33.7501, -84.3885], zoom_start=10, tiles="CartoDB positron")
+for _, r in df_geo.iterrows():
+    # Without simplifying the representation of each borough,
+    # the map might not be displayed
+    sim_geo = geopandas.GeoSeries(r["geometry"]).simplify(tolerance=0.001)
+    geo_j = sim_geo.to_json()
+    geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillColor": "orange"})
+    geo_j.add_to(m)
+m.save('test_geomap.html')
+quit()
 
 #unneccessary
 #geoseries = geopandas.GeoSeries(df_atl_neighborhoods['geometry'], crs="EPSG:4326")
