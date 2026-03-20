ceddc22aee0de6db09f77522db07627cd9b57271
JoeEllis <joeellis.2013@gmail.com>
2026-03-20 13:40:43 -0400

    created functionality to only run geocode on new entries

--- scripts/test.py
+++ scripts/test.py
@@ -1,47 +1,25 @@
-import time
 import pandas
-from geopy.geocoders import Nominatim
-from functools import lru_cache
-import folium
-import re
+import utils_dataframe
+
+old_df = pandas.read_pickle('df_food_geocode.pkl')
+old_df_drop = old_df.drop(['coordinates', 'latitude', 'longitude'], axis=1)
+new_df = utils_dataframe.reformat_sheet('ATL-Food-and-Drink-Review-List.xlsx')
+
+def clean(df):
+    return df.map(lambda x: x.strip().lower() if isinstance(x, str) else x)
+old_df_drop = clean(old_df_drop)
+new_df = clean(new_df)
+
+key_cols = list(new_df.columns)
+#finds new columns (left)
+merge_df = new_df.merge(
+    old_df_drop,
+    on=key_cols,
+    how="left",
+    indicator=True
+)
+
+new_entries = merge_df[merge_df['_merge'] == "left_only"].drop('_merge', axis = 1)
 
 
-@lru_cache(maxsize=None)
-def try_geocode(query, fallback):
-    if fallback == False:
-        try:
-            location = geolocator.geocode(query, country_codes='US')
-            if location:
-                return location.latitude, location.longitude
-        except Exception as e:
-            return None, None
-    else:
-        try:
-            location = lambda query: geolocator.geocode("%s, Georgia, United States" % query)
-            if location:
-                address = location(query)
-                return address.latitude, address.longitude
-        except Exception as e:
-            return None, None
 
-    return None, None
-
-
-def geocode_with_fallback(row):
-    address = clean_address(row.get('Location', ''))
-    name = row.get('Name', '')
-
-    #trys to geocode based on the address
-    name_fallback = False
-    lat, lon = try_geocode(address, name_fallback)
-
-    #if address doesn't work, falls back to trying just restaurant name
-    if lat is None or lon is None:
-        name_fallback = True
-        #uses alternative function
-        lat, lon = try_geocode(name, name_fallback)
-        #if fails again, doesn't geocode
-        if lat is None or lon is None:
-            print(f"WARING: Geocoding failed for both address and name -> '{address}' / '{name}'")
-
-    return lat, lon
